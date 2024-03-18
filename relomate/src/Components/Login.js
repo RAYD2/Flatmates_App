15f2400b2a7380e631d0a44a0e9c41250ccfec81
Hussam Khan <HussamK2812@gmail.com>
2024-03-18 12:40:24 +0000

    Updated login and register pages

--- relomate/src/Components/Login.js
+++ relomate/src/Components/Login.js
@@ -4,9 +4,19 @@ const Login = () => {
     return (
         <div className="login-form">
             <h1 className="header">Login</h1>
-            <form>
-                <input type="text" name="username" placeholder="Username" required></input>
-                <input type="password" name="password" placeholder="Password"></input>
+            <form action="#">
+                <div className="username-box">
+                    <input type="text" name="username" placeholder="Username" required></input>
+                </div>
+                <div className="password-box">
+                    <input type="password" name="password" placeholder="Password" required></input>
+                </div>
+                <div className="login-button">
+                    <button type="submit">Login</button>
+                </div>
+                <div className="register-link">
+                    <a href="Register.js">Click Here</a>
+                </div>
             </form>
         </div>
     )
