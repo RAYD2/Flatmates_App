5a09c072209c4216bffce12e6a6029e1b2c6766a
Hussam Khan <HussamK2812@gmail.com>
2024-03-25 16:58:45 +0000

    begun to add CSS

--- relomate/src/Components/Register.js
+++ relomate/src/Components/Register.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 const Register = () => {
-    const checkPasswordMatch = () => {
-        alert("Hi")
-    }
     return (
         <div className="Register-form">
             <h1 className="header">Register</h1>
@@ -23,7 +20,7 @@ const Register = () => {
                     <input type="password" name="confirmPassword" placeholder="Confirm Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required></input>
                 </div>
                 <div className="login-button">
-                    <button type="submit" onClick={checkPasswordMatch}>Register Account</button>
+                    <button type="submit">Register Account</button>
                 </div>
             </form>
         </div>
