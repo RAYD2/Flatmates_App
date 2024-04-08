a59e78272c62fc322633eb4d243a20a03723fd8f
Shi Jie Chen <98396425+SJChen02@users.noreply.github.com>
2024-04-08 02:57:24 +0100

    Add files via upload
    Updated an example matching employee page (not complete).

--- relomate/src/Components/MatchEmployeeComponent/FindCommonUser.js
+++ relomate/src/Components/MatchEmployeeComponent/FindCommonUser.js
@@ -2,32 +2,93 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../../ComponentStyles/MatchEmployeeCss/MatchingEmployeePage.css";
 
+// change this line. so it can import profile pic from somewhere better
+import img from "../../assets/livingRoom.png"
+
+
 function FindUser(){
     return (
         <>
-
         <div className="ProfileContainer">
-          <div className='ProfilePicContainer'>
-                <div className='Profile_Picture'></div>
-                <div className='Profile_Person_Name'>Name Name</div>
+            <div className='ProfilePicContainer'>
+                {/* /* this line. take care */}
+                <img className="Profile_Picture" src={img} alt="Profile Pics"></img>
+
+                {/* edit this line too to make name change. */}
+                <div className='Profile_Person_Name'>FirstName Surname</div>
+
             </div>
 
-            <div className='Profile_Description'>
-                <div className='AbtMe'>About me</div>
-                <div className='MyDescription'>
+            {/* edit this line too to make description change. */}
+            <div className='Profile_Container'>
+                <div className='Title'>About me</div>
+                <div className='Detail'>
                     Lorem ipsum dolor sit amet, 
                     consectetur adipiscing elit.
-                    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua... More
                 </div>
             </div>
+
+            <div className='Profile_Container'>
+                <div className='Title'>Hobbies</div>
+                <div className='Detail'>
+                    <div>Sport</div>
+                    <div>Drama</div>
+                    <div>Anime</div>
+                    <div>Music</div>
+                    <div>Dance</div>
+                </div>
+            </div>
+
+            <div className='Profile_Container'>
+                <div className='Title'>Location</div>
+                <div className='MyLoc'>XHT101 Apartment</div>
+            </div>
+
         </div>
 
+        <div className="ProfileContainer">
+            <div className='ProfilePicContainer'>
+                {/* /* this line. take care */}
+                <img className="Profile_Picture" src={img} alt="Profile Pics"></img>
+
+                {/* edit this line too to make name change. */}
+                <div className='Profile_Person_Name'>FirstName Surname</div>
+
+            </div>
+
+            {/* edit this line too to make description change. */}
+            <div className='Profile_Container'>
+                <div className='Title'>About me</div>
+                <div className='Detail'>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit.
+                    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua... More
+                </div>
+            </div>
+
+            <div className='Profile_Container'>
+                <div className='Title'>Hobbies</div>
+                <div className='Detail'>
+                    <div>Sport</div>
+                    <div>Drama</div>
+                    <div>Anime</div>
+                    <div>Music</div>
+                    <div>Dance</div>
+                </div>
+            </div>
+
+            <div className='Profile_Container'>
+                <div className='Title'>Location</div>
+                <div className='MyLoc'>None - Not living in Apartment</div>
+            </div>
+
+        </div>
+
+        
+
         </>
-      );
+    );
 }
 
 export default FindUser
